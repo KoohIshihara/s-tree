78ea325a4c676b260fc5a57949421289cac838a5
KouIshihara <as.it.were.kou@gmail.com>
2019-02-05 16:02:31 +0900

    latest: (textarea)

--- public/js/scenario.js
+++ public/js/scenario.js
@@ -11,6 +11,8 @@ function scenarioGetContent(scenario, id) {
 }
 //module.exports.scenarioGetContent = scenarioGetContent;
 
+
+// kooh--------------------
 function scenarioAddContent(scenario, content) {
   scenario.push(content);
 }
@@ -31,6 +33,7 @@ function scenarioConnectFromGroupNode(from, toId) {
   }
   from.selections = selections;
 }
+// kooh--------------------
 
 
 // TO DOあるidのnextを持つsingle nodeを取り出す
@@ -73,6 +76,145 @@ function scenarioDeleteNode(scenario, id) {
     }
 }
 //module.exports.scenarioDeleteNode = scenarioDeleteNode;
+
+
+
+function _disconnectNode(node) {
+  delete node.next;
+  delete node.gui.topLineId;
+}
+
+function scenarioDisconnectNormalNode(scenario, id) {
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'single' && scenario[i].next == id) {
+      delete scenario[i].next;
+      delete scenario[i].gui.topLineId;
+    }
+  }
+}
+//module.exports.scenarioDisconnectNormalNode = scenarioDisconnectNormalNode;
+
+function scenarioDisconnectSelectionsNode(scenario, id) {
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'group') {
+      var selections = scenario[i].selections;
+      for (var j = 0; j < selections.length; j++) {
+        if (selections[j].next == id) {
+          delete selections[j].next;
+          delete selections[j].topLineId;
+        }
+      }
+    }
+  }
+}
+//module.exports.scenarioDisconnectSelectionsNode = scenarioDisconnectSelectionsNode;
+
+function scenarioGetEventById(scenario, id) {
+  var event = null;
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].id == id) {
+      event = scenario[i];
+      break;
+    }
+  }
+  return event;
+}
+//module.exports.scenarioGetEventById = scenarioGetEventById;
+
+function scenarioGetNodeById(scenario, id) {
+  var node = null;
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'single') {
+      if (scenario[i].id == id) {
+        node = scenario[i];
+        break;
+      }
+    } else if (scenario[i].nodeType == 'group') {
+      var selections = scenario[i].selections;
+      for (var j = 0; j < selections.length; j++) {
+        if (selections[j].id == id) {
+          node = selections[j];
+          break;
+        }
+      }
+    }
+  }
+  return node;
+}
+//module.exports.scenarioGetNodeById = scenarioGetNodeById;
+
+// Was: getNormalNodesFromScenarioByNext
+function scenarioGetNodesThatConnectTo(scenario, id) {
+  var nodes = [];
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'single' && scenario[i].next == id) {
+      nodes.push(scenario[i]);
+    }
+  }
+  return nodes;
+}
+//module.exports.scenarioGetNodesThatConnectTo = scenarioGetNodesThatConnectTo;
+
+// Was: getSelectionsFromScenarioByNext
+function scenarioGetSelectionsThatConnectTo(scenario, id) {
+  var results = [];
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'group') {
+      var selections = scenario[i].selections;
+      for (var j = 0; j < selections.length; j++) {
+        if (selections[j].next == id) {
+          results.push(selections[j]);
+        }
+      }
+    }
+  }
+  return results;
+}
+//module.exports.scenarioGetSelectionsThatConnectTo = scenarioGetSelectionsThatConnectTo;
+
+// Was: getIndexesOfNormalNodesFromScenarioByNext
+function scenarioGetNormalNodeIndexesThatConnectTo(scenario, id) {
+  var indexes = [];
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'single' && scenario[i].next == id) {
+      indexes.push({ scenarioIndex: i });
+    }
+  }
+  return indexes;
+}
+//module.exports.scenarioGetNormalNodeIndexesThatConnectTo = scenarioGetNormalNodeIndexesThatConnectTo;
+
+// Was: getIndexesOfSelectionsFromScenarioByNext
+function scenarioGetSelectionIndexesThatConnectTo(scenario, id) {
+  var indexes = [];
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].nodeType == 'group') {
+      var selections = scenario[i].selections;
+      for (var j = 0; j < selections.length; j++) {
+        if (selections[j].next == id) {
+          indexes.push({ scenarioIndex: i, selectionIndex: j });
+        }
+      }
+    }
+  }
+  return indexes;
+}
+//module.exports.scenarioGetSelectionIndexesThatConnectTo = scenarioGetSelectionIndexesThatConnectTo;
+
+// Was: getEventOfGoToByToId
+function scenarioGetGoToEventsThatJumpTo(scenario, id) {
+  var events = [];
+  for (var i = 0; i < scenario.length; i++) {
+    if (scenario[i].type == 'goto' && scenario[i].toId == id) {
+      events.push(scenario[i]);
+    }
+  }
+  return events;
+}
+//module.exports.scenarioGetGoToEventsThatJumpTo = scenarioGetGoToEventsThatJumpTo;
+
+
+
 
 // scenarioHistories
 
